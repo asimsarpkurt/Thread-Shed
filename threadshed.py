7f0197de21b2e36b79b0b36a7588d377e61a89c6
asimsarpkurt <79475614+asimsarpkurt@users.noreply.github.com>
2021-04-14 18:17:27 +0300

    Update threadshed.py

--- threadshed.py
+++ threadshed.py
@@ -7,7 +7,7 @@ Some of my daily responsibilities involve tallying the number of sales during th
 Unfortunately, the Thread Shed has an extremely outdated register system and stores all of the transaction information in one huge unwieldy string called daily_sales.
 All day, for each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. 
 My task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists.
-
+My codes begin from 120th line
 author=@Sarp
 """
 daily_sales = \
