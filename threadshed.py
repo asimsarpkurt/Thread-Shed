d6448be10cbc12ac0ea7a2ad89c2f28a22cd22ee
asimsarpkurt <79475614+asimsarpkurt@users.noreply.github.com>
2021-04-14 18:16:46 +0300

    Update threadshed.py

--- threadshed.py
+++ threadshed.py
@@ -2,13 +2,13 @@
 """
 Spyder Editor
 Thread Shed
-You’ve recently been hired as a cashier at the local sewing hobby shop, Thread Shed. 
-Some of your daily responsibilities involve tallying the number of sales during the day, calculating the total amount of money made, and keeping track of the names of the customers.
+Assume I have recently been hired as a cashier at the local sewing hobby shop, Thread Shed. 
+Some of my daily responsibilities involve tallying the number of sales during the day, calculating the total amount of money made, and keeping track of the names of the customers.
 Unfortunately, the Thread Shed has an extremely outdated register system and stores all of the transaction information in one huge unwieldy string called daily_sales.
 All day, for each transaction, the name of the customer, amount spent, types of thread purchased, and the date of sale is all recorded in this same string. 
-Your task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists.
-This is a temporary script file.
-author=Sarp
+My task is to use your Python skills to iterate through this string and clean up each transaction and store all the information in easier-to-access lists.
+
+author=@Sarp
 """
 daily_sales = \
 """Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
